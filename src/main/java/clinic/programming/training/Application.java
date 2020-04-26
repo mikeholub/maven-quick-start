6725139c74910b49a0e664d43d941a6abde4446e
mikalaiholub <kolyan100@ya.ru>
2020-04-26 13:23:54 -0700

    remove comment

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -6,7 +6,6 @@ public class Application {
         System.out.println ("Inside Application");
     }
 
-    // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
